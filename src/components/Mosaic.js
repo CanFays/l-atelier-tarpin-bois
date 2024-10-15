cb1405d4e1e1fe7b065580e3d8c0ff743650ea21
candice.fays@ymail.com <candice.fays@ymail.com>
2024-10-15 20:47:53 +0200

    mosaic : background et comportement 3*2*3 en sm - ok

--- src/components/Mosaic.js
+++ src/components/Mosaic.js
@@ -3,23 +3,25 @@ import './Mosaic.css';
 import InstaCarousel from '../assets/icons/InstaCarousel.svg'
 
 const Mosaic = () => {
- // Importer toutes les images du dossier "assets/images" qui commencent par "mosaic"
-  const images = require.context('../assets/images', false, /mosaic.*\.(png|jpe?g|webp)$/);
-
-  // Obtenir le chemin des images
+ // toutes les images qui commencent par "mosaic" (png|jpe?g|webp)
+  const images = require.context('../assets/images', false, /mosaic.*\.(webp)$/);
+  // le chemin des images
   const imageList = images.keys().map(images);
 
   return (
     <div className="mosaic-component container">
       <div className="row d-flex justify-content-evenly">
         {imageList.map((image, index) => (
-          <div className="mosaic-item col-5 col-sm-4 col-md-3">
-            <img key={index}
-            className="mosaic-img"
-            src={image}
-            alt={`Création ${index + 1} de Tarpin Bois`} />
+          <>
+          <div key={index + 1} className="mosaic-item col-5 col-sm-4 col-md-3">
+            <img className="mosaic-img"
+                 src={image}
+                 alt={`Création ${index + 1} de Tarpin Bois`} />
             <img className="mosaic-icon" src={InstaCarousel} alt="Instagram carousel" />
           </div>
+          {(index === 4) && <div className="w-100 d-sm-block d-md-none"></div>}
+          </>
+
         ))}
       </div>
     </div>
