89b6533eb9f917020381eee544312740ada21cdf
candice.fays@ymail.com <candice.fays@ymail.com>
2024-10-25 14:37:57 +0200

    carousel comportement prev next comments ok

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,35 +1,59 @@
-import { useState } from 'react';
 import './Carousel.css'
+import { useState } from 'react';
+
+
+
+const comments = [
+  { id: 1, name: "Karine T.", comment: "Pas d'hésitation si vous êtes intéressés par le travail du bois. Benjamin vous fera utiliser de nombreuses machines de son atelier, vous apprendra différentes techniques et vous fera part de sa grande expérience (par exemple reboucher un défaut du bois avec du café 😁), dans une ambiance très sympa. Mon mari a été ravi de l'expérience" },
+  { id: 2, name: "Jean-Christophe R.", comment: "Très belle rencontre avec Benjamin et son atelier très bien équipé, 2 jours de grande qualité, humaine et technique, un vrai régal. Encore merci à toi Benjamin pour ton accueil, et de nous permettre de partager ta passion du bois et tes compétences." },
+  { id: 3, name: "Monique A.", comment: "4 copines réunies pour fabriquer des planches à découper en compagnie d'Igor ; Dès l'accueil Igor nous a mis à l'aise il nous a parlé de sa passion, de l'amour du bois qui lui avait fait changer de métier. Très pédagogue, il nous a montré toutes les étapes de la transformation d'un morceau de bois en une magnifique planche à découper. On a pu utiliser en toute sécurité les machines telles que scie à bandes ,défonceuse, ponceuse. Avec compétence, gentillesse et une pointe d'humour !" },
+  { id: 4, name: "Edouard G.", comment: "Super atelier ! Beaucoup de pédagogie dans les explications ! Les queues d'arronde en plus d'être très esthétiques sont très utiles pour assembler c'est un vrai plaisir de faire cet atelier 🙂 " }
+]
 
 function openCarousel() {
+  console.log("dans open");
+
 }
 
 function Carousel() {
 
-  function closeCarousel() {
+  const [currentId, setcurrentId] = useState(1);
+  console.log("entrée Carousel; id=", currentId, typeof currentId);
+  let currentComment = comments.find(item => item.id === currentId);
+  console.log("entrée Carousel; currentComment :", currentComment);
+
+  function handlePrev() {
+    setcurrentId(currentId === 1 ? comments.length : currentId - 1);
+    console.log("dans prev, id:", currentId);
   }
 
-  const comments = [
-    { name: "Karine T.", comment: "Pas d'hésitation si vous êtes intéressés par le travail du bois. Benjamin vous fera utiliser de nombreuses machines de son atelier, vous apprendra différentes techniques et vous fera part de sa grande expérience (par exemple reboucher un défaut du bois avec du café 😁), dans une ambiance très sympa. Mon mari a été ravi de l'expérience" },
-    { name: "Jean-Christophe R.", comment: "Très belle rencontre avec Benjamin et son atelier très bien équipé, 2 jours de grande qualité, humaine et technique, un vrai régal. Encore merci à toi Benjamin pour ton accueil, et de nous permettre de partager ta passion du bois et tes compétences." },
-    { name: "Monique A.", comment: "4 copines réunies pour fabriquer des planches à découper en compagnie d'Igor ; Dès l'accueil Igor nous a mis à l'aise il nous a parlé de sa passion, de l'amour du bois qui lui avait fait changer de métier. Très pédagogue, il nous a montré toutes les étapes de la transformation d'un morceau de bois en une magnifique planche à découper. On a pu utiliser en toute sécurité les machines telles que scie à bandes ,défonceuse, ponceuse. Avec compétence, gentillesse et une pointe d'humour !" },
-    { name: "Edouard G.", comment: "Super atelier ! Beaucoup de pédagogie dans les explications ! Les queues d'arronde en plus d'être très esthétiques sont très utiles pour assembler c'est un vrai plaisir de faire cet atelier 🙂 " }
-  ]
+  function handleNext() {
+    setcurrentId(currentId === comments.length ? 1 : currentId + 1);
+    console.log("dans next, id:", currentId);
+  }
 
+  function closeCarousel() {
+    console.log("dans close");
+  }
 
 
   return (
     <div className='carousel'>
-      <div className="prev">
+      <div className="btn-prev" onClick={handlePrev}>
         <img src="" alt="Commentaire précédent" />
       </div>
       <div className="content">
-        OW the bioutifoule caroussel !!!
+        <div className="content-name"></div>
+        <div className="content-comment"></div>
+        {currentComment.name}
+        {currentComment.comment}
+
+
       </div>
-      <div className="next">
+      <div className="btn-next" onClick={handleNext}>
         <img src="" alt="Commentaire suivant" />
       </div>
-      <div className="close">
+      <div className="btn-close" onClick={closeCarousel}>
         <img src="" alt="Fermer les ommentaires" />
       </div>
     </div>
