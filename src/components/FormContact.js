db40f0c74af506e6078edc25ecd84d5e04f3aca1
candice.fays@ymail.com <candice.fays@ymail.com>
2024-11-06 16:17:02 +0100

    efface form après envoi, modif des msg envoi ou non

--- src/components/FormContact.js
+++ src/components/FormContact.js
@@ -7,8 +7,8 @@ function FormContact() {
   const form = useRef();
   const serviceId = process.env.REACT_APP_SERVICE_ID;
   const templateId = process.env.REACT_APP_TEMPLATE_ID;
-  const publicKey = process.env.REACT_APP_PUBLIC_KEY;
-  // remettre publicKey = ""
+  const publicKey = "bloup";
+  // remettre publicKey = "process.env.REACT_APP_PUBLIC_KEY"
 
   const sendEmail = (e) => {
     e.preventDefault();
@@ -19,51 +19,42 @@ function FormContact() {
       })
       .then(
         () => {
-          console.log('SUCCESS!');
-          // faire une pop-up et suppr les contenus input
-          alert('Message envoyé')
+          // faire une pop-up ?
+          alert('Message envoyé, nous vous recontactons très bientôt !')
+          document.contactForm.reset();
         },
         (error) => {
-          console.log("ERREUR MAILJS...", error.text);
-          alert("ÉCHEC DE L'ENVOI...", JSON.stringify(error))
+          alert(`Le message n'a pas pu être envoyé, veuillez réessayer ...                 ${JSON.stringify(error)}`)
         },
       );
   };
 
   return (
-    <form ref={form} onSubmit={sendEmail}
-    className=''>
+    <form ref={form} onSubmit={sendEmail} name='contactForm'>
       <p>Vous avez également la possibilité de nous envoyer un message directement :</p>
-
       <div className="row">
         <div className="col-9 col-sm-5 col-md-4">
           <input type="text" name="from_name" id='from_name' placeholder='Nom' autoCapitalize='words'/>
         </div>
       </div>
-
       <div className="row">
         <div className="col-9 col-sm-5 col-md-4">
           <input type="tel" name="from_phone" id='from_phone' placeholder='Téléphone'/>
         </div>
-
         <div className="col-9 col-sm-8 col-md-7">
           <input type="email" name="from_email" id='from_email' placeholder='E-mail'/>
         </div>
       </div>
-
       <div className="row">
         <div className="col-11">
           <textarea name="message" id='message' placeholder='Message'/>
         </div>
       </div>
-
-
-
       <div className="d-flex">
         <input type="submit" value="Go !"
         className="btn-tommette " />
-        <div id='mail-fail' className="d-none">Votre message n'a pas pu être envoyé ...</div>
-        <div id='mail-success' className="d-none">Votre message a bien été envoyé. À bientôt !</div>
+        {/* <div id='mail-fail' className="d-none">Votre message n'a pas pu être envoyé ...</div>
+        <div id='mail-success' className="d-none">Votre message a bien été envoyé. À bientôt !</div> */}
       </div>
     </form>
   );
