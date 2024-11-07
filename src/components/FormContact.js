584af99b691e40f2820be70afea73b54e3bbf459
candice.fays@ymail.com <candice.fays@ymail.com>
2024-11-07 23:16:00 +0100

    comportement du form : name et message required, phone ou email demandé avec popup si vide - ok

--- src/components/FormContact.js
+++ src/components/FormContact.js
@@ -4,37 +4,50 @@ import emailjs from '@emailjs/browser';
 
 function FormContact() {
 
-  const form = useRef();
-  const serviceId = process.env.REACT_APP_SERVICE_ID;
-  const templateId = process.env.REACT_APP_TEMPLATE_ID;
-  const publicKey = "bloup";
-  // remettre publicKey = "process.env.REACT_APP_PUBLIC_KEY"
+  const formRef = useRef();
+  const popupRef = useRef();
+  const serviceId ="service_407g23s";
+  // const serviceId = process.env.REACT_APP_SERVICE_ID;
+  // remettre serviceId = bon mail dans template de emailJs !!!
+  const templateId = "template_6ltmfh5";
+  // const templateId = process.env.REACT_APP_TEMPLATE_ID;
+  const publicKey = "Dw36i0bdWOpKwn1x7";
+  // const publicKey = process.env.REACT_APP_PUBLIC_KEY;
 
   const sendEmail = (e) => {
     e.preventDefault();
 
-    emailjs
-      .sendForm(serviceId, templateId, form.current, {
-        publicKey: publicKey,
-      })
-      .then(
-        () => {
-          // faire une pop-up ?
-          alert('Message envoyé, nous vous recontactons très bientôt !')
-          document.contactForm.reset();
-        },
-        (error) => {
-          alert(`Le message n'a pas pu être envoyé, veuillez réessayer ...                 ${JSON.stringify(error)}`)
-        },
-      );
+    console.log('tel :', document.contactForm.from_phone.value === "")
+    console.log('mail :', document.contactForm.from_email.value === "")
+
+    if (document.contactForm.from_email.value === "" && document.contactForm.from_phone.value === "") {
+      console.log("popup à faire : champs vides");
+      popupRef.current.classList.remove('d-none');
+    } else {
+
+      emailjs
+        .sendForm(serviceId, templateId, formRef.current, {
+          publicKey: publicKey,
+        })
+        .then(
+          () => {
+            alert('Message envoyé, nous vous recontactons très bientôt !')
+            document.contactForm.reset();
+            popupRef.current.classList.add('d-none');
+          },
+          (error) => {
+            alert(`Le message n'a pas pu être envoyé, veuillez réessayer ...   ${JSON.stringify(error)}`)
+          },
+        );
+    }
   };
 
   return (
-    <form ref={form} onSubmit={sendEmail} name='contactForm'>
+    <form ref={formRef} onSubmit={sendEmail} name='contactForm'>
       <p>Vous avez également la possibilité de nous envoyer un message directement :</p>
       <div className="row">
         <div className="col-9 col-sm-5 col-md-4">
-          <input type="text" name="from_name" id='from_name' placeholder='Nom' autoCapitalize='words'/>
+          <input type="text" name="from_name" id='from_name' placeholder='Nom' autoCapitalize='words' required/>
         </div>
       </div>
       <div className="row">
@@ -45,16 +58,15 @@ function FormContact() {
           <input type="email" name="from_email" id='from_email' placeholder='E-mail'/>
         </div>
       </div>
+      <div className="popup--give-contact d-none" ref={popupRef}>Donnez-nous un moyen de vous recontacter : e-mail ou téléphone !</div>
       <div className="row">
         <div className="col-11">
-          <textarea name="message" id='message' placeholder='Message'/>
+          <textarea name="message" id='message' placeholder='Message' required/>
         </div>
       </div>
       <div className="d-flex">
         <input type="submit" value="Go !"
         className="btn-tommette " />
-        {/* <div id='mail-fail' className="d-none">Votre message n'a pas pu être envoyé ...</div>
-        <div id='mail-success' className="d-none">Votre message a bien été envoyé. À bientôt !</div> */}
       </div>
     </form>
   );
